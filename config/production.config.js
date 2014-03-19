b804282b769d173f8418daeb3155d00ebd5c395e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-19 21:05:04 +0200

    reduced config

--- config/production.config.js
+++ config/production.config.js
@@ -1,128 +1,19 @@
 var config = {
-	connection: process.env.MONGO_CONNECTION,
-	options: { auto_reconnect: true },
-
-	applicationUrl: 'https://app.likeastore.com',
-	siteUrl: 'https://likeastore.com',
-	domain: '.likeastore.com',
-
-	auth: {
-		cookieName: 'auth_token',
-		signKey: 'c88afe1f6aa4b3c7982695ddd1cdd200bcd96662',
-		tokenTtl: 10080, // minutes, 7 days
-		secure: true
-	},
-
-	hashids: {
-		salt: '0b208b34946d64c41a11bab4eb34a7c6515ac2e9'
-	},
-
-	tracking: {
-		enabled: true
-	},
-
-	// api keys
-	services: {
-		github: {
-			appId: process.env.GITHUB_APP_ID,
-			appSecret: process.env.GITHUB_APP_SECRET
-		},
-
-		twitter: {
-			consumerKey: process.env.TWITTER_CONSUMER_KEY,
-			consumerSecret: process.env.TWITTER_CONSUMER_SECRET
-		},
-
-		facebook: {
-			appId: process.env.FACEBOOK_APP_ID,
-			appSecret: process.env.FACEBOOK_APP_SECRET
-		},
-
-		stackoverflow: {
-			clientId: process.env.STACKOVERFLOW_CLIENT_ID,
-			clientKey: process.env.STACKOVERFLOW_CLIENT_KEY,
-			clientSecret: process.env.STACKOVERFLOW_CLIENT_SECRET
-		},
-
-		vimeo: {
-			clientId: process.env.VIMEO_CLIENT_ID,
-			clientSecret: process.env.VIMEO_CLIENT_SECRET
-		},
-
-		youtube: {
-			clientId: process.env.YOUTUBE_CLIENT_ID,
-			clientSecret: process.env.YOUTUBE_CLIENT_SECRET
-		},
-
-		behance: {
-			clientId: process.env.BEHANCE_CLIENT_ID,
-			clientSecret: process.env.BEHANCE_CLIENT_SECRET
-		},
-
-		vk: {
-			clientId: process.env.VK_CLIENT_ID,
-			clientSecret: process.env.VK_CLIENT_SECRET
-		},
-
-		pocket: {
-			consumerKey: process.env.POCKET_CONSUMER_KEY
-		},
-
-		tumblr: {
-			consumerKey: process.env.TUMBLR_CONSUMER_KEY,
-			consumerSecret: process.env.TUMBLR_CONSUMER_SECRET,
-		}
-	},
+	connection: 'mongodb://localhost:27017/likeastoredb',
 
 	mandrill: {
-		token: process.env.MANDRILL_TOKEN
+		token: '2kXX0stV1Hf56y9DYZts3A'
 	},
 
 	logentries: {
-		token: process.env.LOGENTRIES_TOKEN
+		token: null
 	},
 
 	analytics: {
-		url: 'https://analytics.likeastore.com',
-		application: 'likeastore-production',
+		url: 'http://localhost:3005',
+		application: 'likeastore-development',
 		username: 'likeastore',
-		password: 'likeadmin7analitics'
-	},
-
-	ga: {
-		id: 'UA-41034999-1',
-		domain: 'likeastore.com'
-	},
-
-	newrelic: {
-		application: 'likeastore-app',
-		licenseKey: 'e5862474ee62b99898c861dddfbfa8a89ac54f49'
-	},
-
-	collector: {
-		// scheduler cycle
-		schedulerRestart: 1000,
-
-		// after collector got to normal mode, next scheduled run in 15 mins
-		nextNormalRunAfter: 1000 * 60 * 15,
-
-		// after collector got to rateLimit mode, next scheduled run in hour
-		nextRateLimitRunAfter: 1000 * 60 * 60,
-
-		// initial mode quotes
-		quotes: {
-			github: {
-				runAfter: 5000
-			},
-
-			twitter: {
-				runAfter: 60000
-			},
-
-			stackoverflow: {
-				runAfter: 5000
-			}
-		}
+		password: 'mypass'
 	}
 };
 
--- config/test.config.js
+++ config/test.config.js
@@ -1,80 +1,8 @@
 var config = {
-	connection: 'mongodb://localhost:27017/likeastoredbtest',
-	options: { auto_reconnect: true },
-
-	applicationUrl: 'http://localhost:3001',
-	siteUrl: 'http://localhost:3000',
-	domain: '',
-
-	auth: {
-		signKey: 'c88afe1f6aa4b3c7982695ddd1cdd200bcd96662',
-		tokenTtl: 10080, // minutes, 7 days
-		secure: false
-	},
-
-	hashids: {
-		salt: '0b208b34946d64c41a11bab4eb34a7c6515ac2e9'
-	},
-
-	tracking: {
-		enabled: false
-	},
-
-	// api keys
-	services: {
-		github: {
-			appId: 'dc3c7a7050dccee24ed3',
-			appSecret: 'c18dde90f5e928a39b0f0432d5125a3e0a31a23d'
-		},
-
-		twitter: {
-			consumerKey: 'dgwuxgGb07ymueGJF0ug',
-			consumerSecret: 'eusoZYiUldYqtI2SwK9MJNbiygCWOp9lQX7i5gnpWU'
-		},
-
-		facebook: {
-			appId: '394024317362081',
-			appSecret: 'bc86f2ab9afcb1227227146e5ea9ad44'
-		},
-
-		stackoverflow: {
-			clientId: '1533',
-			clientKey: 'J2wyheThU5jYFiOpGG22Eg((',
-			clientSecret: 'KOCBFY4OUP6OE7Q1xNw1wA(('
-		},
-
-		vimeo: {
-			clientId: 'd445a0de20a3b178b0422ad0c6d5891bdfd00b97',
-			clientSecret: 'e8e0008413ae1d1ed3e45d8c89d7943ad3937167'
-		},
-
-		youtube: {
-			clientId: '955769903356-5f1407fo9efvljm3hhl5b8mbhos61blq.apps.googleusercontent.com',
-			clientSecret: 'QtlyTnCusfX7G7fbjaEkdmHK'
-		},
-
-		behance: {
-			clientId: 'JyyJsEZRbcqTXcukjnq8ivQMb7BfAIUd',
-			clientSecret: 'L2s8uQl3s7G5uy2ECeRp9dHeWuyA6mrj'
-		},
-
-		vk: {
-			clientId: '4195920',
-			clientSecret: '1IATKWbudSUmp49uVfsn'
-		},
-
-		pocket: {
-			consumerKey: '24341-1a1bc9c0ad0f3ffa9eb3194b'
-		},
-
-		tumblr: {
-			consumerKey: '6vUnFztIzNd6ISG8kBn7UyhGkHA8a49UjXUx9rCYbrWBnbFZBr',
-			consumerSecret: 'pnUrbwgmLHubWqaBxRIzD216FxAq8wZCzf2hXysL9huV1Sfq9R'
-		}
-	},
+	connection: 'mongodb://localhost:27017/likeastoredb',
 
 	mandrill: {
-		token: null
+		token: '2kXX0stV1Hf56y9DYZts3A'
 	},
 
 	logentries: {
@@ -83,45 +11,9 @@ var config = {
 
 	analytics: {
 		url: 'http://localhost:3005',
-		application: 'likeastore-test',
+		application: 'likeastore-development',
 		username: 'likeastore',
 		password: 'mypass'
-	},
-
-	ga: {
-		id: 'UA-41034999-1',
-		domain: 'localhost'
-	},
-
-	newrelic: {
-		application: 'likeastore-app-test',
-		licenseKey: null
-	},
-
-	collector: {
-		// scheduler cycle
-		schedulerRestart: 1000,
-
-		// after collector got to normal mode, next scheduled run in 15 mins
-		nextNormalRunAfter: 10000,
-
-		// after collector got to rateLimit mode, next scheduled run in hour
-		nextRateLimitRunAfter: 1000 * 60 * 60,
-
-		// initial mode quotes
-		quotes: {
-			github: {
-				runAfter: 5000
-			},
-
-			twitter: {
-				runAfter: 60000
-			},
-
-			stackoverflow: {
-				runAfter: 5000
-			}
-		}
 	}
 };
 
