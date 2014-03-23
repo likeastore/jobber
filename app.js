059b200d34b0efb3c5c246348d557ed534746b97
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-23 14:23:43 +0200

    agenda applied

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
+var Agenda = require('agenda');
 var pulse = require('./source/jobs/pulse');
-var moment = require('moment');
+var logger = require('./source/utils/logger');
+var config = require('./config');
 
-var current = moment();
+var agenda = new Agenda({db: {address: config.connection, collection: 'jobs' }, maxConcurrency: 1});
 
-pulse('day', function (err, results) {
-	if (err) {
-		return console.error('pulse failed', err);
-	}
-
-	var done = moment();
-
-	console.log('pulse succeeded, timing:' + moment.duration(done.diff(current)).asMilliseconds(), results);
+agenda.define('daily pulse', function (job, callback) {
+	pulse('day', callback);
 });
+
+agenda.define('weekly pulse', function (job, callback) {
+	pulse('week', callback);
+});
+
+agenda.every('1 minute', 'daily pulse');
+agenda.every('1 minute', 'weekly pulse');
+
+agenda.on('start', function (job) {
+	logger.info({message: 'job started', job: job});
+});
+
+agenda.on('complete', function (job) {
+	logger.success({message: 'job compeleted', job: job});
+});
+
+agenda.on('fail', function (err, job) {
+	logger.error({message: 'job failed', job: job, err: err});
+});
+
+agenda.start();
