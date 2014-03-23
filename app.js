2fa1397d75e57024d5d05313e5a9b5811079cea6
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-03-23 17:18:41 +0200

    purge and redefine

--- app.js
+++ app.js
@@ -7,34 +7,36 @@ var timing = require('./source/utils/timing');
 
 var agenda = new Agenda({db: {address: config.connection, collection: 'jobs'} });
 
-agenda.define('daily pulse', function (job, callback) {
-	pulse('day', callback);
+agenda.purge(function () {
+	agenda.define('measure pulse day', function (job, callback) {
+		pulse('day', callback);
+	});
+
+	agenda.define('measure pulse week', function (job, callback) {
+		pulse('week', callback);
+	});
+
+	agenda.define('measure pulse month', function (job, callback) {
+		pulse('month', callback);
+	});
+
+	agenda.every('1 minute', 'measure pulse day');
+	agenda.every('5 minute', 'measure pulse week');
+	agenda.every('10 minutes', 'measure pulse month');
+
+	agenda.on('start', function (job) {
+		timing.start(job.attrs.name);
+		logger.info({message: 'job started', job: job.attrs.name });
+	});
+
+	agenda.on('success', function (job) {
+		var duration = timing.finish(job.attrs.name);
+		logger.success({message: 'job compeleted', job: job.attrs.name, duration: duration.asMilliseconds()});
+	});
+
+	agenda.on('fail', function (err, job) {
+		logger.error({message: 'job failed', job: job.attrs.name, err: err});
+	});
+
+	agenda.start();
 });
-
-agenda.define('weekly pulse', function (job, callback) {
-	pulse('week', callback);
-});
-
-agenda.define('month pulse', function (job, callback) {
-	pulse('month', callback);
-});
-
-// agenda.every('1 minute', 'daily pulse');
-// agenda.every('1 minute', 'weekly pulse');
-agenda.every('1 minute', 'month pulse');
-
-agenda.on('start', function (job) {
-	timing.start(job.attrs.name);
-	logger.info({message: 'job started', job: job.attrs.name});
-});
-
-agenda.on('success', function (job) {
-	var duration = timing.finish(job.attrs.name);
-	logger.success({message: 'job compeleted', job: job.attrs.name, duration: duration.asMilliseconds()});
-});
-
-agenda.on('fail', function (err, job) {
-	logger.error({message: 'job failed', job: job.attrs.name, err: err});
-});
-
-agenda.start();
