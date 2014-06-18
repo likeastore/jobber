0f2da69c465fde4331bc176666d3fa977b995944
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-06-18 13:56:29 +0300

    fixed db connector

--- source/db/mongo/index.js
+++ source/db/mongo/index.js
@@ -1,7 +1,7 @@
 var mongo = require('mongojs');
 
-module.exports = function (config) {
-	var connection = config.connection || 'mongodb://localhost:27017/likeastoredb';
+module.exports = function (config, connection) {
+	connection = connection || 'connection';
 	var db = mongo.connect(config[connection], ['events', 'actions', 'states', 'collections', 'users', 'items', 'networks', 'emails']);
 	if (!db) {
 		throw new Error('could not connect to ' + connection);
