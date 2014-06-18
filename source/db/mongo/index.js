b533480ce34ef9005659a27edc9f45a03272f86e
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-06-18 14:01:11 +0300

    pulse collection added

--- source/db/mongo/index.js
+++ source/db/mongo/index.js
@@ -2,7 +2,7 @@ var mongo = require('mongojs');
 
 module.exports = function (config, connection) {
 	connection = connection || 'connection';
-	var db = mongo.connect(config[connection], ['events', 'actions', 'states', 'collections', 'users', 'items', 'networks', 'emails']);
+	var db = mongo.connect(config[connection], ['events', 'actions', 'states', 'collections', 'users', 'items', 'networks', 'emails', 'pulse']);
 	if (!db) {
 		throw new Error('could not connect to ' + connection);
 	}
